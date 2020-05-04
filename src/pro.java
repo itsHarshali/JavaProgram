de696fd571c46a147a4e94fdec26e7a874224c9b
Shubham Badgujar <35478603+shubham8550@users.noreply.github.com>
2020-05-04 23:16:41 +0530

    create the properties file, To get information from the properties file

---
+++ src/pro.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class pro {
+    public static void main(String[] args)throws Exception{
+        FileReader reader=new FileReader("pro.properties");
+
+        Properties p=new Properties();
+        p.load(reader);
+
+        System.out.println(p.getProperty("name"));
+    }
+}
+
